e3bbb626f28e2eb319e5ebf782be0925db02954b
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-13 08:04:37 -0300

    Create basic Landing page

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles'
+
+function Landing() {
+    return (
+        <View style={styles.container}>
+            <Text>Hello World Again!</Text>
+        </View>
+    )
+
+}
+
+export default Landing;
