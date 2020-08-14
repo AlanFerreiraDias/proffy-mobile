0f7c190db21a6b437abb77c40dc21e1e09b43ef4
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-14 07:21:25 -0300

    Create global file for colors

---
+++ colors.ts
@@ -0,0 +1,11 @@
+const colors =
+{
+    backGround: '#8257e5',
+    title: '#FFF',
+    buttonBackground: '#333',
+    buttonPrimaryBackGround: '#9871f5',
+    buttonSecondaryBackGround: '#04d361',
+    buttonText: '#FFF'
+}
+
+export default colors
