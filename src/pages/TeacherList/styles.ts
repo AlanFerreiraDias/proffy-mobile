32dc8ffa488e89fb92a17aceb6b10629473d1d68
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-15 08:43:17 -0300

    Create TeacherList Page

---
+++ src/pages/TeacherList/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+import colors from '../../../colors'
+
+const teacherList = colors.teacherList
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: teacherList.backGround
+    },
+})
+
+export default styles
