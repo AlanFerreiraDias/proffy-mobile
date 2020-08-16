9af9405f22ee6a610b994dd2aae2406077efc85f
AlanFerreiraDias <dias.ferreira.alan@gmail.com>
2020-08-16 08:10:55 -0300

    Add Proffy server connection and get total connections

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler'
@@ -9,8 +9,21 @@ import landingImg from '../../assets/images/landing.png'
 import studyIcon from '../../assets/images/icons/study.png'
 import giveClassesIcon from '../../assets/images/icons/give-classes.png'
 import heartIcon from '../../assets/images/icons/heart.png'
+import api from '../../services/api';
 
 function Landing() {
+
+
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data;
+            setTotalConnections(total);
+        })
+    }, []);
+
+
     const { navigate } = useNavigation();
 
     function handleNavigateToTeachPage() {
@@ -46,7 +59,7 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 250 connecções já feitas. {' '}
+                Total de {totalConnections} connecções já feitas. {' '}
                 <Image source={heartIcon} />
             </Text>
 
